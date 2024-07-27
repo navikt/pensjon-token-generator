d0ae60b6bc08634cdd6c84fcc62a007ff0daec40
Per Christian Moen <per.christian.moen@nav.no>
2024-07-27 09:32:36 +0200

    Rendre token på backend@

--- src/main/kotlin/no/nav/pensjon/EndpointController.kt
+++ src/main/kotlin/no/nav/pensjon/EndpointController.kt
@@ -1,8 +1,6 @@
 package no.nav.pensjon
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.ResponseEntity
-import org.springframework.http.ResponseEntity.ok
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
 import org.springframework.stereotype.Controller
@@ -16,26 +14,26 @@ class EndpointController(
     val maskinportenScopes: List<String>,
     val maskinportenTokenService: MaskinportenTokenService,
 ) {
+    val scopeTree: Map<String, Map<String, List<String>>> = maskinportenScopes
+        .sorted()
+        .groupBy { it.split(":")[0] }
+        .map {
+            it.key to it.value.groupBy { scope ->
+                val split = scope.split(":", "/")
+                if (split.size > 1) split[1] else split[0]
+            }
+        }.toMap()
+
     @GetMapping("/")
     fun index(
         model: Model,
+        @RequestParam("scopes", required = false) scopes: List<String>?,
         @AuthenticationPrincipal principal: DefaultOidcUser
     ): String {
-        val scopeTree: Map<String, Map<String, List<String>>> = maskinportenScopes
-            .sorted()
-            .groupBy { it.split(":")[0] }
-            .map {
-                it.key to it.value.groupBy { scope ->
-                    val split = scope.split(":", "/")
-                    if (split.size > 1) split[1] else split[0]
-                }
-            }.toMap()
-
         model.addAttribute("scopeTree", scopeTree)
+        scopes?.let {
+            model.addAttribute("token", maskinportenTokenService.accessToken(it, emptyList(), null))
+        }
         return "index"
     }
-
-    @GetMapping("/token")
-    fun getToken(@RequestParam("scopes") scopes: List<String>): ResponseEntity<String> =
-        ok(maskinportenTokenService.accessToken(scopes, emptyList(), null))
 }
