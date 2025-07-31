327c290d401d67ba2273f9333beb8cde245e9e15
Hans Magnus Hallaraker <hans.magnus.hallaraker@nav.no>
2025-07-31 14:33:18 +0200

    Oppdaterer gradle og kotlin versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    kotlin("plugin.spring") version "2.0.20"
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.spring") version "2.2.0"
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
 }
@@ -9,24 +9,21 @@ group = "no.nav.pensjon-saksbehandling"
 version = "1.0"
 description = "pensjon-maskinporten-test"
 
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("net.logstash.logback", "logstash-logback-encoder", "7.3")
-    implementation("org.springframework.boot", "spring-boot-starter-actuator")
-    implementation("org.springframework.boot", "spring-boot-starter-oauth2-client")
-    implementation("org.springframework.boot", "spring-boot-starter-security")
-    implementation("org.springframework.boot", "spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot", "spring-boot-starter-web")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.13.4")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
-
-tasks.jar{
+tasks.jar {
     manifest {
         attributes("Main-Class" to "Application.AppKt")
     }
